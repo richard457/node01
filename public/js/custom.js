171a9d4f381d87c91b464121e24b0d368ee7a9ae
Muberangabo stapin <mbstapin7@gmail.com>
2017-01-14 23:40:50 +0200

    map

--- public/js/custom.js
+++ public/js/custom.js
@@ -187,4 +187,18 @@ jQuery(function($){
 	    return false;
 	  });
 
+		$('#map').addClass('scrolloff');                // set the mouse events to none when doc is ready
+
+		$('#googleMap').on("mouseup",function(){          // lock it when mouse up
+				$('#map').addClass('scrolloff');
+				//somehow the mouseup event doesn't get call...
+		});
+		$('#googleMap').on("mousedown",function(){        // when mouse down, set the mouse events free
+				$('#map').removeClass('scrolloff');
+		});
+		$("#map").mouseleave(function () {              // becuase the mouse up doesn't work...
+				$('#map').addClass('scrolloff');            // set the pointer events to none when mouse leaves the map area
+																							// or you can do it on some other event
+		});
+
 });
