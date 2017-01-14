22d132d3de3d12514982bca168b28e3b15518eba
Muberangabo stapin <mbstapin7@gmail.com>
2017-01-14 20:35:38 +0200

    removed error

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var http = require('http'),
+var http = require('http');
 var exphbs = require('express-handlebars');
 var db = require('./Database/Db.js');
 var path = require('path');
